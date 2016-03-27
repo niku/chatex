bcab2f1ae106042c8b196cca53e0ab67f4e44f7e
niku <niku@niku.name>
2016-03-27 22:15:38 +0900

    Integrate `tsc --watch` with `mix phoenix.server`

--- config/dev.exs
+++ config/dev.exs
@@ -11,7 +11,7 @@ config :chatex, Chatex.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: []
+  watchers: [node: ["node_modules/typescript/bin/tsc", "--watch", "--pretty"]]
 
 # Watch static and templates for browser reloading.
 config :chatex, Chatex.Endpoint,
